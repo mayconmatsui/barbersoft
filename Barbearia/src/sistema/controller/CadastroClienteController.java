1806b56a60344cb72171031c28466a1cdc352bad
Maycon Matsui <mayconmatsui@gmail.com>
2019-11-29 11:34:48 -0400

    Adicionado tela de cadastro de cliente

---
+++ Barbearia/src/sistema/controller/CadastroClienteController.java
@@ -0,0 +1,22 @@
+package sistema.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author matsui
+ */
+public class CadastroClienteController implements Initializable {
+
+  /**
+   * Initializes the controller class.
+   */
+  @Override
+  public void initialize(URL url, ResourceBundle rb) {
+    // TODO
+  }  
+  
+}
