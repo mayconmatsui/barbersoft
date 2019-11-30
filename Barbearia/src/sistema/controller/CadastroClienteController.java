1022bddfdbe52ade77c681b8256d1a658ff07537
ezequiel <ezequiel@ezequiel-300E5EV-300E4EV-270E5EV-270E4EV-2470EV>
2019-11-29 22:04:47 -0300

    Adicionado os nomes dos atributos

--- Barbearia/src/sistema/controller/CadastroClienteController.java
+++ Barbearia/src/sistema/controller/CadastroClienteController.java
@@ -2,7 +2,12 @@ package sistema.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -14,6 +19,42 @@ public class CadastroClienteController implements Initializable {
   /**
    * Initializes the controller class.
    */
+    @FXML
+    private TextField tfRG;
+
+    @FXML
+    private DatePicker dpDataNasc;
+
+    @FXML
+    private TextField tfTelefone;
+
+    @FXML
+    private TextField tfCPF;
+
+    @FXML
+    private TextField tfEndereco;
+
+    @FXML
+    private TextField tfEmail;
+
+    @FXML
+    private Button btNovo;
+
+    @FXML
+    private Button btCancelar;
+
+    @FXML
+    private TextField tfNome;
+
+    @FXML
+    void novo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cancelar(ActionEvent event) {
+
+    }
   @Override
   public void initialize(URL url, ResourceBundle rb) {
     // TODO
