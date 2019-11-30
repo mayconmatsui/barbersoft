ba1d87b965c4eacd4088423b37c12660fd98f875
ezequiel <ezequiel@ezequiel-300E5EV-300E4EV-270E5EV-270E4EV-2470EV>
2019-11-29 22:40:01 -0300

    Adicionado os nomes dos atributos

--- Barbearia/src/sistema/controller/ListaClientesController.java
+++ Barbearia/src/sistema/controller/ListaClientesController.java
@@ -7,18 +7,57 @@ package sistema.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
  *
  * @author matsui
  */
+
 public class ListaClientesController implements Initializable {
 
   /**
    * Initializes the controller class.
    */
+    @FXML
+    private Button btBusca;
+
+    @FXML
+    private Button btExcluir;
+
+    @FXML
+    private TextField tfBusca;
+
+    @FXML
+    private Button btEditar;
+
+    @FXML
+    private Button btNovo;
+
+    @FXML
+    private TableColumn<?, ?> tbClientes;
+
+    @FXML
+    void buscar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void novo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void editar(ActionEvent event) {
+
+    }
+
   @Override
   public void initialize(URL url, ResourceBundle rb) {
     // TODO
--- Barbearia/src/sistema/controller/PrincipalController.java
+++ Barbearia/src/sistema/controller/PrincipalController.java
@@ -7,7 +7,11 @@ package sistema.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
 
 /**
  * FXML Controller class
@@ -19,6 +23,109 @@ public class PrincipalController implements Initializable {
   /**
    * Initializes the controller class.
    */
+    @FXML
+    private MenuItem miEfetuarVenda;
+
+    @FXML
+    private MenuItem miClientes;
+
+    @FXML
+    private MenuItem miAnivesariantes;
+
+    @FXML
+    private Menu mbRelatorios;
+
+    @FXML
+    private MenuItem miSaidaEstoque;
+
+    @FXML
+    private Menu mbAgenda;
+
+    @FXML
+    private Menu mbVendas;
+
+    @FXML
+    private MenuItem miProdutos;
+
+    @FXML
+    private MenuItem miAgendamentos;
+
+    @FXML
+    private MenuItem miFuncionarios;
+
+    @FXML
+    private MenuItem miRelatorioProdutos;
+
+    @FXML
+    private MenuItem miCategorias;
+
+    @FXML
+    private MenuItem miEntradaEstoque;
+
+    @FXML
+    private Menu mbEstoque;
+
+    @FXML
+    private MenuItem miRelatorioVendas;
+
+    @FXML
+    private Menu mbCadastros;
+
+    @FXML
+    void abrirCategorias(ActionEvent event) {
+
+    }
+
+    @FXML
+    void abrirClientes(ActionEvent event) {
+
+    }
+
+    @FXML
+    void abrirFuncionarios(ActionEvent event) {
+
+    }
+
+    @FXML
+    void abrirProdutos(ActionEvent event) {
+
+    }
+
+    @FXML
+    void abrirAgendamentos(ActionEvent event) {
+
+    }
+
+    @FXML
+    void abrirVendas(ActionEvent event) {
+
+    }
+
+    @FXML
+    void AbrirEntradaEstoque(ActionEvent event) {
+
+    }
+
+    @FXML
+    void abrirSaidaEstoque(ActionEvent event) {
+
+    }
+
+    @FXML
+    void abrirAniversariantes(ActionEvent event) {
+
+    }
+
+    @FXML
+    void abrirRelatorioProdutos(ActionEvent event) {
+
+    }
+
+    @FXML
+    void abrirRelatorioVendas(ActionEvent event) {
+
+    }
+
   @Override
   public void initialize(URL url, ResourceBundle rb) {
     // TODO
